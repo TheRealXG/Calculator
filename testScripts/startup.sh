a24ac9736b7066791c3ecaed9abdc1c0903e8489
James Brewer <james.e.brewer34.civ@cvr.mil>
2021-02-25 15:14:25 -0600

    Updated build command to clean and build

--- testScripts/startup.sh
+++ testScripts/startup.sh
@@ -8,7 +8,7 @@ cd /volume
 
 #configure waf to build exe with correct rtems dir and BSP / then build
 ./waf configure --rtems=/rtems/quick-start/rtems/5 --rtems-bsp=arm/realview_pbx_a9_qemu
-build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir sonarcloud ./waf
+build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir sonarcloud ./waf clean build
 
 
 #Run the Robot test framework on all *.robot files in testScripts. Output in main Calculator folder.
