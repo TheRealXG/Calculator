f2025e32e8086e8fe801aaabfb4f464d9f857d0c
James Brewer <james.e.brewer34.civ@cvr.mil>
2021-03-01 12:51:10 -0600

    Don't list branch is sonar command if master

--- testScripts/startup.sh
+++ testScripts/startup.sh
@@ -17,8 +17,13 @@ build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir build_wrapper_outpu
 # Add to path and execute Sonarcloud Scan (This is downloaded in the pipeline. Should it be in Docker instead?)
 # This path is dictated by the command line in the pipeline to download and extract the sonar-scanner
 export PATH=$PATH:$SRC_VOL/sonar/bin
-# Pass in the Git Branch name (provided by Azure) being porcessed for Sonarcloud to track from 
-sonar-scanner -Dsonar.login=$SC_AUTH -Dsonar.branch.name=$GIT_BRANCH
+# Pass in the Git Branch name (provided by Azure) being processed for Sonarcloud to track from
+if [$GIT_BRANCH = 'master']
+then
+	sonar-scanner -Dsonar.login=$SC_AUTH
+else
+	sonar-scanner -Dsonar.login=$SC_AUTH -Dsonar.branch.name=$GIT_BRANCH
+fi
 
 #Run the Robot test framework on all *.robot files in testScripts. Output in main Calculator folder.
 robot --xunit results testScripts/.
