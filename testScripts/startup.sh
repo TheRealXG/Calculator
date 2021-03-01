fde5ea4b58c158bcf61fdccff61784d96f5b67fc
James Brewer <james.e.brewer34.civ@cvr.mil>
2021-03-01 12:16:04 -0600

    Added sonar command property for current branch

--- testScripts/startup.sh
+++ testScripts/startup.sh
@@ -17,7 +17,8 @@ build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir build_wrapper_outpu
 # Add to path and execute Sonarcloud Scan (This is downloaded in the pipeline. Should it be in Docker instead?)
 # This path is dictated by the command line in the pipeline to download and extract the sonar-scanner
 export PATH=$PATH:$SRC_VOL/sonar/bin
-sonar-scanner -Dsonar.login=$SC_AUTH
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+sonar-scanner -Dsonar.login=$SC_AUTH -Dsonar.branch.name=$branch
 
 #Run the Robot test framework on all *.robot files in testScripts. Output in main Calculator folder.
 robot --xunit results testScripts/.
