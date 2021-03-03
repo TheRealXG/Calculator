09073d9d285a0d2c3a694b4947f9a843e31ae7f2
Jim Brewer <j2brewer@gmail.com>
2021-03-03 11:49:53 -0600

    Updated location of Sonar build wrapper

--- testScripts/startup.sh
+++ testScripts/startup.sh
@@ -15,7 +15,7 @@ cd $SRC_VOL
 #configure waf to build exe with correct rtems dir and BSP / then build
 ./waf configure --rtems="${RTEMS_PATH}/quick-start/rtems/5" --rtems-bsp=arm/realview_pbx_a9_qemu
 # Build the application with the SonarCloud wrapper around it (copied by the pipeline into the main agent working directory) 
-../build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory ./waf clean build
+build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory ./waf clean build
 # Copy the final binary into the top level source directory for use by later containers
 cp build/arm-rtems5-realview_pbx_a9_qemu/rtems/*.exe .
 
